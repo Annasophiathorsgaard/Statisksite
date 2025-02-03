4cef77054e7df71b01f458d3b1220867aded8aeb
Annasophiathorsgaard <athorsgaard@icloud.com>
2025-02-03 14:48:20 +0100

    add category list to index

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("index script loaded");
+
+fetch(`https://kea-alt-del.dk/t7/api/categories`)
+.then((response) => response.json())
+.then(showCategori);
+
+function showCategori(data){
+    console.log("min data er: ", data);
+
+    const markup = data
+    .map(
+        (element) => 
+            `<article>
+            <a href="produktliste.html">${element.category}</a>
+            </article>`
+)
+.join("");
+
+console.log("min markup er ", markup);
+document.querySelector("ul").innerHTML = markup;
+}
